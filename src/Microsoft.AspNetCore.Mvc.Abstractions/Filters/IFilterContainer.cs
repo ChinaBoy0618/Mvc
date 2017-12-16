501daa15541edc0eefd4d5804c44343a6950e887
ChinaBoy0618 <826679571@qq.com>
2017-12-16 15:01:54 +0800

    Update IFilterContainer.cs

--- src/Microsoft.AspNetCore.Mvc.Abstractions/Filters/IFilterContainer.cs
+++ src/Microsoft.AspNetCore.Mvc.Abstractions/Filters/IFilterContainer.cs
@@ -5,11 +5,13 @@ namespace Microsoft.AspNetCore.Mvc.Filters
 {
     /// <summary>
     /// A filter that requires a reference back to the <see cref="IFilterFactory"/> that created it.
+    /// 一个过滤器必须有一个工厂去创建它（约束）
     /// </summary>
     public interface IFilterContainer
     {
         /// <summary>
         /// The <see cref="IFilterFactory"/> that created this filter instance.
+        /// 工厂创建这个实例
         /// </summary>
         IFilterMetadata FilterDefinition { get; set; }
     }
